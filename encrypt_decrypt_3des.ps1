e1d5598b0361545c44e601d8e2fb4d7c4e9e65d6
caspersonnr <nathaniel.casperson@navy.mil>
2020-07-27 16:29:21 -0700

    File add

---
+++ encrypt_decrypt_3des.ps1
@@ -0,0 +1,57 @@
+###############################################################
+# Decrypt or Encrypt Script - 3DES
+#
+# Created by Nathaniel Casperson nathaniel.casperson@gmail.com
+###############################################################
+
+#####################
+#[Reflection.Assembly]::LoadWithPartialName("System.Security")
+#####################
+
+Function Protect-String-3DES($String, $Passphrase, $InitVector) {
+    $Key = [Text.Encoding]::UTF8.GetBytes($Passphrase)
+    $IV = [Text.Encoding]::UTF8.GetBytes($InitVector)
+            
+    $cryptoProvider = New-Object System.Security.Cryptography.TripleDESCryptoServiceProvider
+    $memoryStream = New-Object IO.MemoryStream    
+    $cryptoStream = New-Object Security.Cryptography.CryptoStream $memoryStream,$cryptoProvider.CreateEncryptor($Key,$IV),"Write"
+    $streamWriter = New-Object IO.StreamWriter $cryptostream
+    $streamWriter.Write($String)
+    
+    $streamWriter.Close()
+    $cryptoStream.Close()
+    $memoryStream.Close()
+    $cryptoProvider.Clear()
+    
+    [byte[]]$result = $memoryStream.ToArray()
+    if($arrayOutput) {
+        return $result
+    } else {
+        Write-Output "This is our encrypted string "
+        return [Convert]::ToBase64String($result)
+    }    
+} # end of function
+
+Function Unprotect-String-3DES($Encrypted, $Passphrase, $InitVector) {
+    if($Encrypted -is [string]){
+        $Encrypted = [Convert]::FromBase64String($Encrypted)
+    }
+
+    $cryptoProvider = New-Object System.Security.Cryptography.TripleDESCryptoServiceProvider
+    $Key = [Text.Encoding]::UTF8.GetBytes($Passphrase)
+    $IV = [Text.Encoding]::UTF8.GetBytes($InitVector)
+    
+    $memoryStream = New-Object IO.MemoryStream @(,$Encrypted)
+    $cryptoStream = New-Object Security.Cryptography.CryptoStream $memoryStream,$cryptoProvider.CreateDecryptor($Key, $IV),"Read"
+    $streamReader = New-Object IO.StreamReader $cryptoStream
+    Write-Output "This is the decrypted string "
+    return $streamReader.ReadToEnd()
+    $streamReader.Close()
+    $cryptoStream.Close()
+    $memoryStream.Close()
+    $cryptoProvider.Clear()   
+} # end of function
+
+
+Protect-String-3DES "What you talkin' about Willis?" "My_Shibboleth_2010_12_31" "31427207"
+Unprotect-String-3DES "UBvVKwXo24JeF45BQx5jEvKNDFjaNl+Kf3Avxt1iiLI=" "My_Shibboleth_2010_12_31" "31427207"
